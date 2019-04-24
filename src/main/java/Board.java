0324975c74e93de91a38f363660eef402b7f5b4b
kaerdhalis <benjamin.leguillou@heig-vd.ch>
2019-04-24 12:35:39 +0200

    modification board avec tests

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,18 +1,34 @@
+import java.util.ArrayList;
+
 public class Board {
 
-    private Square[] board;
+    private ArrayList<Square> board;
 
     public Board() {
-        board = new Square[40];
+        board = new ArrayList<>(40);
 
-        board[0] = new Square("Go");
+        board.add(0, new Square("Go"));
         for (int i = 1; i < 40; i++) {
-            board[i] = new Square("Square " + i);
+            board.add(i, new Square("Square " + i));
         }
     }
      public Square StartCase() {
 
-             return board[0];
+             return board.get(0);
+        }
+
+
+        public Square newPosition(Square previous ,int value){
+
+        int index = board.indexOf(previous);
+
+        return board.get((index+value)%40);
+
+        }
+
+        public Square getSquare(int index){
+
+        return board.get(index);
         }
 
 
